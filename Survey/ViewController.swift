23842898da35b13fcff5480f3eaada3f52f73645
zacalex <qinjiahuangus@gmail.com>
2017-10-09 02:00:21 -0700

    Initial Commit

---
+++ Survey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Survey
+//
+//  Created by Qinjia Huang on 10/9/17.
+//  Copyright © 2017 Qinjia Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
